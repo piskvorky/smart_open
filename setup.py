bfbc82509da7bef2f1ef78a28baa4872f56c5854
Radim Řehůřek <radimrehurek@seznam.cz>
2015-01-24 22:19:24 +0100

    update release version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(fname):
 
 setup(
     name = 'smart_open',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Utils for streaming large files (S3, HDFS, gzip, bz2...)',
     long_description = read('README.rst'),
 
