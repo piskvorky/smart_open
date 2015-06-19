ddac517bb60ae16449faae4eccd73e28c68941eb
salilb <salilb@outlook.com>
2015-06-19 10:34:12 -0700

    Fixing syntax bug in s3_iter_bucket tests

--- smart_open/tests/test_smart_open.py
+++ smart_open/tests/test_smart_open.py
@@ -463,14 +463,14 @@ class S3IterBucketTest(unittest.TestCase):
 
         # read all keys + their content back, in parallel, using s3_iter_bucket
         result = {}
-        for k, c in smart_open.s3_iter_bucket(mybucket)
+        for k, c in smart_open.s3_iter_bucket(mybucket):
             result[k.name] = c
         #result = dict(smart_open.s3_iter_bucket(mybucket))
         self.assertEqual(expected, result)
 
         # read some of the keys back, in parallel, using s3_iter_bucket
         result = {}
-        for k, c in smart_open.s3_iter_bucket(mybucket, accept_key=lambda fname: fname.endswith('4'))
+        for k, c in smart_open.s3_iter_bucket(mybucket, accept_key=lambda fname: fname.endswith('4')):
             result[k.name] = c
         #result = dict(smart_open.s3_iter_bucket(mybucket, accept_key=lambda fname: fname.endswith('4')))
         self.assertEqual(result, dict((k, c) for k, c in expected.items() if k.endswith('4')))
