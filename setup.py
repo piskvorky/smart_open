62d58123a6fca0a5387d2454474407fded2d21bf
Lev Konstantinovskiy <lev.konst@gmail.com>
2016-04-25 17:45:34 +0100

    MIT License Trove

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: Public Domain',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
