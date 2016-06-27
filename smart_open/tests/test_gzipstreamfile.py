a62691e8836863fb93610f5c531cb4ed19bf917f
Michael Penkov <misha.penkov@gmail.com>
2016-06-27 23:14:14 +0300

    fixing gzipstream tests to work with Python 3.x
    By default, the open() built-in does character encoding, so we need to
    explicitly specify that we want bytes (open in binary mode).

--- smart_open/tests/test_gzipstreamfile.py
+++ smart_open/tests/test_gzipstreamfile.py
@@ -10,18 +10,20 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_warc_md5sum(self):
         """Does the WARC md5 checksum match the expected value?"""
+        fpath = P.join(CURR_DIR, 'test_data/crlf_at_1k_boundary.warc.gz')
         for read_size in [1, 2, 10, 28, 42, 100, 256, 512, 800]:
-            f = open(P.join(CURR_DIR, 'test_data/crlf_at_1k_boundary.warc.gz'))
+            f = open(fpath, "rb")
             gz = gzipstream.gzipstreamfile.GzipStreamFile(f)
             data = []
             tmp = gz.read(read_size)
             while tmp:
                 data.append(tmp)
                 tmp = gz.read(read_size)
-            data = ''.join(data)
+            data = b''.join(data)
             m = hashlib.md5(data)
             assert m.hexdigest() == '18473e60f8c7c98d29d65bf805736a0d', \
-                'Failed with read size of {}'.format(read_size)
+                'Failed with read size of {0}'.format(read_size)
+            f.close()
 
 if __name__ == '__main__':
     unittest.main()
