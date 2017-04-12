4c0a9170f2cd46bc1e2ca7e41b7aa4d656154a48
tmylk <lev.konst@gmail.com>
2017-04-12 09:19:12 -0300

    Bump up version to 1.5.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(fname):
 
 setup(
     name = 'smart_open',
-    version = '1.5.1',
+    version = '1.5.2',
     description = 'Utils for streaming large files (S3, HDFS, gzip, bz2...)',
     long_description = read('README.rst'),
 
