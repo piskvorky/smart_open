0ba9b93d99e68ef3b74c0e3858c642b157ee2f8a
ivan <menshikh.iv@gmail.com>
2017-10-15 19:56:47 +0500

    Fix setup.py, remove useless requirements*.txt

--- setup.py
+++ setup.py
@@ -9,40 +9,34 @@
 
 import io
 import os
-import sys
-
-if sys.version_info < (2, 6):
-    raise ImportError("smart_open requires python >= 2.6")
-
-
-# TODO add ez_setup?
 from setuptools import setup, find_packages
+
 
 def read(fname):
     return io.open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 
 setup(
-    name = 'smart_open',
-    version = '1.5.3',
-    description = 'Utils for streaming large files (S3, HDFS, gzip, bz2...)',
-    long_description = read('README.rst'),
+    name='smart_open',
+    version='1.5.3',
+    description='Utils for streaming large files (S3, HDFS, gzip, bz2...)',
+    long_description=read('README.rst'),
 
     packages=find_packages(),
     package_data={"smart_open.tests": ["test_data/*gz"]},
 
-    author = u'Radim Řehůřek',
-    author_email = 'me@radimrehurek.com',
-    maintainer = u'Radim Řehůřek',
-    maintainer_email = 'me@radimrehurek.com',
+    author=u'Radim Řehůřek',
+    author_email='me@radimrehurek.com',
+    maintainer=u'Radim Řehůřek',
+    maintainer_email='me@radimrehurek.com',
 
-    url = 'https://github.com/piskvorky/smart_open',
-    download_url = 'http://pypi.python.org/pypi/smart_open',
+    url='https://github.com/piskvorky/smart_open',
+    download_url='http://pypi.python.org/pypi/smart_open',
 
-    keywords = 'file streaming, s3, hdfs',
+    keywords='file streaming, s3, hdfs',
 
-    license = 'MIT',
-    platforms = 'any',
+    license='MIT',
+    platforms='any',
 
     install_requires=[
         'boto >= 2.32',
@@ -59,13 +53,12 @@ setup(
 
     test_suite="smart_open.tests",
 
-    classifiers = [ # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
