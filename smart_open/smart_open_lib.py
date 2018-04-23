455d722a7c4d54fd884aae0f40e6359a24a7ffc7
Michael Penkov <misha.penkov@gmail.com>
2018-04-23 09:10:32 +0900

    add comment to explain hacky code

--- smart_open/smart_open_lib.py
+++ smart_open/smart_open_lib.py
@@ -95,7 +95,14 @@ Uri = collections.namedtuple(
 """Represents all the options that we parse from user input.
 
 Some of the above options only make sense for certain protocols, e.g.
-bucket_id is only for S3."""
+bucket_id is only for S3.
+"""
+#
+# Set the default values for all Uri fields to be None.  This allows us to only
+# specify the relevant fields when constructing a Uri.
+#
+# https://stackoverflow.com/questions/11351032/namedtuple-and-default-values-for-optional-keyword-arguments
+#
 Uri.__new__.__defaults__ = (None,) * len(Uri._fields)
 
 
