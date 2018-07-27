bd6dbdb4574ec942096eb551f5a6af5fabbce120
Michael Penkov <misha.penkov@gmail.com>
2018-07-27 16:27:35 +0900

    get rid of unnecessary code

--- integration-tests/test_209.py
+++ integration-tests/test_209.py
@@ -22,35 +22,6 @@ if _NUMROWS is not None:
     _NUMROWS = int(_NUMROWS)
 
 
-def is_ascii(s):
-    return all(ord(c) < 128 for c in s)
-
-
-def checkAvroSchema(ss):
-    flag = 0
-    lowerLetters = "abcdefghijklmnopqrstuvwxyz"
-    upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    digits = "0123456789"
-    s = list(ss)
-    if s[0] in lowerLetters or s[0] in upperLetters:
-        pass
-    else:
-        s.insert(0, '_')
-    for ii, c in enumerate(s):
-        if c in lowerLetters or c in upperLetters or c in digits:
-            pass
-        elif not is_ascii(c):
-            flag = 1
-            s[ii] = ''
-        else:
-            s[ii] = '_'
-    if flag == 1:
-        ss = "".join(s)+'_JPN'
-    else:
-        ss = "".join(s)
-    return ss.replace(' ', '_').upper()
-
-
 def gen_schema(paramNames, dataName, paramTypes=''):
     paramNamesLen = len(paramNames)
     paramTypeLen = len(paramTypes)
@@ -89,12 +60,10 @@ if not P.isfile('index_2018.csv'):
     os.system('aws s3 cp s3://irs-form-990/index_2018.csv .')
 
 with open('index_2018.csv') as fin:
-    data = pn.read_csv(fin, header=1, error_bad_lines=False,
-                       nrows=_NUMROWS).fillna('NA')
+    data = pn.read_csv(fin, header=0, error_bad_lines=False,
+                       nrows=_NUMROWS, dtype='str').fillna('NA')
 
-paramNames = [checkAvroSchema(x) for x in list(data.columns)]
-dataName = 'NAME'
-avroSchemaOut = gen_schema(paramNames, dataName=dataName, paramTypes='')
+avroSchemaOut = gen_schema(data.columns, dataName='data', paramTypes='')
 
 with open('schema.out', 'wb') as fout:
     fout.write(avroSchemaOut)
