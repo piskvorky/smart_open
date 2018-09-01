72ae8ee1d59006d2a6f544c35fc584dcecb29ff7
ivan <menshikh.iv@gmail.com>
2018-09-01 18:17:06 +0500

    drop 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -68,8 +68,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: System :: Distributed Computing',
