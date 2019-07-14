7d94968e15116ab28b202c27b1dce834652ca3fc
Michael Penkov <misha.penkov@gmail.com>
2019-07-15 00:51:17 +0900

    clean up unit tests, remove duplication

--- smart_open/tests/test_s3_version.py
+++ smart_open/tests/test_s3_version.py
@@ -61,62 +61,64 @@ def put_to_bucket(contents, num_attempts=12, sleep_time=5):
 class TestVersionId(unittest.TestCase):
 
     def setUp(self):
-        self.WRITE_KEY_NAME = 'test-write-key-{}'.format(uuid.uuid4().hex)
+        #
+        # Each run of this test reuses the BUCKET_NAME, but works with a
+        # different key for isolation.
+        #
+        self.key = 'test-write-key-{}'.format(uuid.uuid4().hex)
+        self.url = "s3://%s/%s" % (BUCKET_NAME, self.key)
         self.test_ver1 = u"String version 1.0".encode('utf8')
         self.test_ver2 = u"String version 2.0".encode('utf8')
-        # write into key
-        with open("s3://"+BUCKET_NAME+"/"+self.WRITE_KEY_NAME, 'wb') as fout:
+
+        with open(self.url, 'wb') as fout:
             fout.write(self.test_ver1)
-        with open("s3://" + BUCKET_NAME + "/" + self.WRITE_KEY_NAME, 'wb') as fout:
+        with open(self.url, 'wb') as fout:
             fout.write(self.test_ver2)
+
+        #
+        # Earlist versions will be first in the list.
+        #
+        self.versions = [
+            v.id
+            for v in boto3.resource('s3').Bucket(BUCKET_NAME).object_versions.filter(Prefix=self.key)
+        ]
 
     def test_good_id(self):
         """Does passing the version_id parameter into the s3 submodule work correctly when reading?"""
-        versions = boto3.resource('s3').Bucket(BUCKET_NAME).object_versions.filter(Prefix=self.WRITE_KEY_NAME)
-        check_version = list(versions)[0].get()['VersionId']
-        transport_params = {'version_id': check_version}
-        with open("s3://%s/%s" % (BUCKET_NAME, self.WRITE_KEY_NAME),
-                  mode='rb', transport_params=transport_params) as fin:
+        params = {'version_id': self.versions[0]}
+        with open(self.url, mode='rb', transport_params=params) as fin:
             expected = fin.read()
         self.assertEqual(expected, self.test_ver1)
 
     def test_bad_id(self):
         """Does passing an invalid version_id exception into the s3 submodule get handled correctly?"""
-        transport_params = {'version_id': 'bad-version-does-not-exist'}
+        params = {'version_id': 'bad-version-does-not-exist'}
         with self.assertRaises(IOError):
-            open("s3://%s/%s" % (BUCKET_NAME, self.WRITE_KEY_NAME), 'rb', transport_params=transport_params)
+            open(self.url, 'rb', transport_params=params)
 
     def test_bad_mode(self):
         """Do we correctly handle non-None version when writing?"""
-        versions = boto3.resource('s3').Bucket(BUCKET_NAME).object_versions.filter(Prefix=self.WRITE_KEY_NAME)
-        check_version = list(versions)[0].get()['VersionId']
-        transport_params = {'version_id': check_version}
+        params = {'version_id': self.versions[0]}
         with self.assertRaises(ValueError):
-            open("s3://%s/%s" % (BUCKET_NAME, self.WRITE_KEY_NAME), 'wb', transport_params=transport_params)
+            open(self.url, 'wb', transport_params=params)
 
     def test_no_version(self):
         """Passing in no version at all gives the newest version of the file?"""
-        with open("s3://%s/%s" % (BUCKET_NAME, self.WRITE_KEY_NAME), 'rb') as fin:
+        with open(self.url, 'rb') as fin:
             expected = fin.read()
         self.assertEqual(expected, self.test_ver2)
 
     def test_newest_version(self):
-        """Passing in the newest version explicitly gives the same as above?"""
-        versions = boto3.resource('s3').Bucket(BUCKET_NAME).object_versions.filter(Prefix=self.WRITE_KEY_NAME)
-        newest_version = list(versions)[-1].get()['VersionId']
-        transport_params = {'version_id': newest_version}
-        with open("s3://%s/%s" % (BUCKET_NAME, self.WRITE_KEY_NAME),
-                  mode='rb', transport_params=transport_params) as fin:
+        """Passing in the newest version explicitly gives the most recent content?"""
+        params = {'version_id': self.versions[1]}
+        with open(self.url, mode='rb', transport_params=params) as fin:
             expected = fin.read()
         self.assertEqual(expected, self.test_ver2)
 
     def test_oldset_version(self):
-        """Passing in the oldest version gives you the oldest version?"""
-        versions = boto3.resource('s3').Bucket(BUCKET_NAME).object_versions.filter(Prefix=self.WRITE_KEY_NAME)
-        oldest_version = list(versions)[0].get()['VersionId']
-        transport_params = {'version_id': oldest_version}
-        with open("s3://%s/%s" % (BUCKET_NAME, self.WRITE_KEY_NAME),
-                  mode='rb', transport_params=transport_params) as fin:
+        """Passing in the oldest version gives the oldest content?"""
+        params = {'version_id': self.versions[0]}
+        with open(self.url, mode='rb', transport_params=params) as fin:
             expected = fin.read()
         self.assertEqual(expected, self.test_ver1)
 
