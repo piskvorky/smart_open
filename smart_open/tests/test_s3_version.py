4c751b7305a98032e8ad63650ec5e335d1f77d13
Michael Penkov <misha.penkov@gmail.com>
2019-07-17 22:16:36 +0900

    chasing race condition in tests

--- smart_open/tests/test_s3_version.py
+++ smart_open/tests/test_s3_version.py
@@ -57,6 +57,14 @@ def put_to_bucket(contents, num_attempts=12, sleep_time=5):
     assert False, 'failed to create bucket %s after %d attempts' % (BUCKET_NAME, num_attempts)
 
 
+def get_versions(bucket, key):
+    """Return object versions in chronological order."""
+    return [
+        v.id
+        for v in boto3.resource('s3').Bucket(bucket).object_versions.filter(Prefix=key)
+    ]
+
+
 @maybe_mock_s3
 class TestVersionId(unittest.TestCase):
 
@@ -72,23 +80,29 @@ class TestVersionId(unittest.TestCase):
 
         with open(self.url, 'wb') as fout:
             fout.write(self.test_ver1)
+
+        logging.info('versions after first write: %r', get_versions(BUCKET_NAME, self.key))
+
+        if DISABLE_MOCKS:
+            #
+            # I suspect there is a race condition that's messing up the
+            # order of the versions in the test.
+            #
+            time.sleep(1)
+
         with open(self.url, 'wb') as fout:
             fout.write(self.test_ver2)
 
-        #
-        # Earlist versions will be first in the list.
-        #
-        self.versions = [
-            v.id
-            for v in boto3.resource('s3').Bucket(BUCKET_NAME).object_versions.filter(Prefix=self.key)
-        ]
+        self.versions = get_versions(BUCKET_NAME, self.key)
+        logging.info('versions after second write: %r', get_versions(BUCKET_NAME, self.key))
+
 
     def test_good_id(self):
         """Does passing the version_id parameter into the s3 submodule work correctly when reading?"""
         params = {'version_id': self.versions[0]}
         with open(self.url, mode='rb', transport_params=params) as fin:
-            expected = fin.read()
-        self.assertEqual(expected, self.test_ver1)
+            actual = fin.read()
+        self.assertEqual(actual, self.test_ver1)
 
     def test_bad_id(self):
         """Does passing an invalid version_id exception into the s3 submodule get handled correctly?"""
@@ -105,22 +119,22 @@ class TestVersionId(unittest.TestCase):
     def test_no_version(self):
         """Passing in no version at all gives the newest version of the file?"""
         with open(self.url, 'rb') as fin:
-            expected = fin.read()
-        self.assertEqual(expected, self.test_ver2)
+            actual = fin.read()
+        self.assertEqual(actual, self.test_ver2)
 
     def test_newest_version(self):
         """Passing in the newest version explicitly gives the most recent content?"""
         params = {'version_id': self.versions[1]}
         with open(self.url, mode='rb', transport_params=params) as fin:
-            expected = fin.read()
-        self.assertEqual(expected, self.test_ver2)
+            actual = fin.read()
+        self.assertEqual(actual, self.test_ver2)
 
     def test_oldset_version(self):
         """Passing in the oldest version gives the oldest content?"""
         params = {'version_id': self.versions[0]}
         with open(self.url, mode='rb', transport_params=params) as fin:
-            expected = fin.read()
-        self.assertEqual(expected, self.test_ver1)
+            actual = fin.read()
+        self.assertEqual(actual, self.test_ver1)
 
 
 if __name__ == '__main__':
