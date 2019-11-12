45e1019caa66cd8768725a77d868c14ea6f3784d
Peter Dannemann <pdannemann@bdtrust.org>
2019-11-11 20:44:15 -0500

    Start updating setup.py and README

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ install_requires = [
     'boto >= 2.32',
     'requests',
     'boto3',
+    'google-cloud-storage',
 ]
 if sys.version_info[0] == 2:
     install_requires.append('bz2file')
@@ -54,7 +55,7 @@ if sys.version_info[0] == 2:
 setup(
     name='smart_open',
     version=_get_version(),
-    description='Utils for streaming large files (S3, HDFS, gzip, bz2...)',
+    description='Utils for streaming large files (S3, HDFS, GCS, gzip, bz2...)',
     long_description=read('README.rst'),
 
     packages=find_packages(),
@@ -71,7 +72,7 @@ setup(
     url='https://github.com/piskvorky/smart_open',
     download_url='http://pypi.python.org/pypi/smart_open',
 
-    keywords='file streaming, s3, hdfs',
+    keywords='file streaming, s3, hdfs, gcs',
 
     license='MIT',
     platforms='any',
