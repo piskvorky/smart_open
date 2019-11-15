a5e2a6eabd868f9b0c1e36d0e96acdb5def7bf9f
Peter Dannemann <pdannemann@bdtrust.org>
2019-11-15 09:00:27 -0500

    Add gcs integration tests

---
+++ integration-tests/test_gcs.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import io
+import os
+
+from google.cloud import storage
+
+import smart_open
+
+_GCS_BUCKET = os.environ.get('SO_GCS_BUCKET')
+_GCS_URL = 'gs://' + _GCS_BUCKET
+assert _GCS_BUCKET is not None, 'please set the SO_GCS_BUCKET environment variable'
+
+
+def initialize_bucket():
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(_GCS_BUCKET)
+    blobs = bucket.list_blobs()
+    for blob in blobs:
+        blob.delete()
+
+
+def write_read(key, content, write_mode, read_mode, encoding=None, **kwargs):
+    with smart_open.smart_open(key, write_mode, encoding=encoding, **kwargs) as fout:
+        fout.write(content)
+    with smart_open.smart_open(key, read_mode, encoding=encoding, **kwargs) as fin:
+        actual = fin.read()
+    return actual
+
+
+def read_length_prefixed_messages(key, read_mode, encoding=None, **kwargs):
+    with smart_open.smart_open(key, read_mode, encoding=encoding, **kwargs) as fin:
+        actual = b''
+        length_byte = fin.read(1);
+        while len(length_byte):
+            actual += length_byte
+            msg = fin.read(ord(length_byte))
+            actual += msg
+            length_byte = fin.read(1)
+    return actual
+
+
+def test_gcs_readwrite_text(benchmark):
+    initialize_bucket()
+
+    key = _GCS_URL + '/sanity.txt'
+    text = 'с гранатою в кармане, с чекою в руке'
+    actual = benchmark(write_read, key, text, 'w', 'r', 'utf-8')
+    assert actual == text
+
+
+def test_gcs_readwrite_text_gzip(benchmark):
+    initialize_bucket()
+
+    key = _GCS_URL + '/sanity.txt.gz'
+    text = 'не чайки здесь запели на знакомом языке'
+    actual = benchmark(write_read, key, text, 'w', 'r', 'utf-8')
+    assert actual == text
+
+
+def test_gcs_readwrite_binary(benchmark):
+    initialize_bucket()
+
+    key = _GCS_URL + '/sanity.txt'
+    binary = b'this is a test'
+    actual = benchmark(write_read, key, binary, 'wb', 'rb')
+    assert actual == binary
+
+
+def test_gcs_readwrite_binary_gzip(benchmark):
+    initialize_bucket()
+
+    key = _GCS_URL + '/sanity.txt.gz'
+    binary = b'this is a test'
+    actual = benchmark(write_read, key, binary, 'wb', 'rb')
+    assert actual == binary
+
+
+def test_gcs_performance(benchmark):
+    initialize_bucket()
+
+    one_megabyte = io.BytesIO()
+    for _ in range(1024*128):
+        one_megabyte.write(b'01234567')
+    one_megabyte = one_megabyte.getvalue()
+
+    key = _GCS_URL + '/performance.txt'
+    actual = benchmark(write_read, key, one_megabyte, 'wb', 'rb')
+    assert actual == one_megabyte
+
+
+def test_gcs_performance_gz(benchmark):
+    initialize_bucket()
+
+    one_megabyte = io.BytesIO()
+    for _ in range(1024*128):
+        one_megabyte.write(b'01234567')
+    one_megabyte = one_megabyte.getvalue()
+
+    key = _GCS_URL + '/performance.txt.gz'
+    actual = benchmark(write_read, key, one_megabyte, 'wb', 'rb')
+    assert actual == one_megabyte
+
+def test_gcs_performance_small_reads(benchmark):
+    initialize_bucket()
+
+    ONE_MIB = 1024**2
+    one_megabyte_of_msgs = io.BytesIO()
+    msg = b'\x0f' + b'0123456789abcde' # a length-prefixed "message"
+    for _ in range(0, ONE_MIB, len(msg)):
+            one_megabyte_of_msgs.write(msg)
+    one_megabyte_of_msgs = one_megabyte_of_msgs.getvalue()
+
+    key = _GCS_URL + '/many_reads_performance.bin'
+
+    with smart_open.smart_open(key, 'wb') as fout:
+        fout.write(one_megabyte_of_msgs)
+
+    actual = benchmark(read_length_prefixed_messages, key, 'rb', buffer_size=ONE_MIB)
+    assert actual == one_megabyte_of_msgs
+
+# def test_gcs_encrypted_file(benchmark):
+#     initialize_bucket()
+#
+#     key = _GCS_URL + '/sanity.txt'
+#     text = 'с гранатою в кармане, с чекою в руке'
+#     actual = benchmark(write_read, key, text, 'w', 'r', 'utf-8', s3_upload={
+#         'ServerSideEncryption': 'AES256'
+#     })
+#     assert actual == text
