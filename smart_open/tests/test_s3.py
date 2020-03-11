7aef60795d855c4b3d7db0c75e1f76b515475e1d
Michael Penkov <m@penkov.dev>
2020-03-11 15:31:32 +1100

    Revert "disable test_old when mocks are disabled"
    This reverts commit 65065626c35e90c821d1eff246314d6def00c13f.

--- smart_open/tests/test_s3.py
+++ smart_open/tests/test_s3.py
@@ -460,7 +460,6 @@ class IterBucketTest(unittest.TestCase):
         expected = ['key_%d' % x for x in range(num_keys)]
         self.assertEqual(sorted(keys), sorted(expected))
 
-    @unittest.skipIf(DISABLE_MOCKS, 'this test mysteriously fails when mocks are disabled')
     def test_old(self):
         """Does s3_iter_bucket work correctly?"""
         #
