7a75e36e32911d69ae2ceb5ba2cd65ac95654e56
Michael Penkov <m@penkov.dev>
2020-03-12 15:57:17 +1100

    update sanity test

--- smart_open/tests/test_sanity.py
+++ smart_open/tests/test_sanity.py
@@ -4,10 +4,37 @@ import boto3
 import moto
 
 
+@moto.mock_s3()
+def setUpModule():
+    bucket = boto3.resource('s3').create_bucket(Bucket='mybucket')
+    bucket.wait_until_exists()
+
+
+@moto.mock_s3()
+def tearDownModule():
+    resource = boto3.resource('s3')
+    bucket = resource.Bucket('mybucket')
+    try:
+        bucket.delete()
+    except resource.meta.client.exceptions.NoSuchBucket:
+        pass
+    bucket.wait_until_not_exists()
+
+
+@moto.mock_s3()
 class Test(unittest.TestCase):
 
-    @moto.mock_s3()
     def test(self):
         resource = boto3.resource('s3')
-        resource.create_bucket(Bucket='mybucket')
-        resource.Bucket('mybucket').delete()
+
+        bucket = resource.Bucket('mybucket')
+        self.assertEqual(bucket.name, 'mybucket')
+
+        expected = b'hello'
+        resource.Object('mybucket', 'mykey').put(Body=expected)
+
+        actual = resource.Object('mybucket', 'mykey').get()['Body'].read()
+        self.assertEqual(expected, actual)
+
+    def tearDown(self):
+        boto3.resource('s3').Object('mybucket', 'mykey').delete()
