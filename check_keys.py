a57eef85221d001db06602f10aa03fa6b561a235
Michael Penkov <m@penkov.dev>
2020-03-12 19:22:11 +1100

    update check_keys.py

--- check_keys.py
+++ check_keys.py
@@ -1,18 +1,40 @@
 """Check that the environment variables contain valid boto3 credentials."""
+import logging
 import os
 import boto3
 
 
-def main():
+def check_implicit():
+    client = boto3.client('s3')
+    try:
+        response = client.list_buckets()
+    except Exception as e:
+        logging.exception(e)
+    else:
+        print([b['Name'] for b in response['Buckets']])
+        print('implicit check OK')
+
+
+def check_explicit():
     key_id = os.environ.get('AWS_ACCESS_KEY_ID')
     secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
     if not (key_id and secret_key):
-        print('no credentials found in os.environ, skipping test')
+        print('no credentials found in os.environ, skipping explicit check')
         return
 
     client = boto3.client('s3', aws_access_key_id=key_id, aws_secret_access_key=secret_key)
-    client.list_buckets()
-    print('OK')
+    try:
+        response = client.list_buckets()
+    except Exception as e:
+        logging.exception(e)
+    else:
+        print([b['Name'] for b in response['Buckets']])
+        print('explicit check OK')
+
+
+def main():
+    check_implicit()
+    check_explicit()
 
 
 if __name__ == '__main__':
