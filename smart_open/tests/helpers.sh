cccfde02d3cb2d61aca3314b528cdc0c708eb64d
Ivan Menshikh <menshikh.iv@gmail.com>
2020-03-24 08:08:04 +0300

    create bash helpers, make tox moar readable

---
+++ smart_open/tests/helpers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+benchmark(){
+  if [[ "$TRAVIS_SECURE_ENV_VARS" = "true" && "$RUN_BENCHMARKS" = "true" ]]; then
+      export SO_S3_URL=$SO_S3_URL/$(python -c "from uuid import uuid4;print(uuid4())");
+      export COMMIT_HASH=$(git rev-parse HEAD);
+
+      set -e;
+      pytest integration-tests/test_s3.py --benchmark-save="$COMMIT_HASH";
+      set +e;
+
+      aws s3 cp .benchmarks/*/*.json "$SO_S3_RESULT_URL";
+      aws s3 rm --recursive $SO_S3_URL;
+  fi
+}
+
+integration(){
+  pytest integration-tests/test_http.py integration-tests/test_207.py
+  if [[ "$TRAVIS_SECURE_ENV_VARS" = "true" ]]; then
+    pytest integration-tests/test_s3_ported.py;
+  fi
+}
+
+"$@"
