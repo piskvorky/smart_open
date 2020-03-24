66a6aef21339b20bb3cb7cf52b1d8c8b0f98927d
Ivan Menshikh <menshikh.iv@gmail.com>
2020-03-24 09:50:46 +0300

    fail explicitly if something goes wrong

--- travis_ci_helpers.sh
+++ travis_ci_helpers.sh
@@ -8,40 +8,41 @@ is_travis_secure_vars_available(){
   if [[ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]]; then
     return 0
   else
+    echo "[WARNING] TRAVIS_SECURE_ENV_VARS=${TRAVIS_SECURE_ENV_VARS} (but should be true)"
     return 1
   fi
 }
 
 benchmark(){
-  if is_travis_secure_vars_available; then
-    SO_S3_URL="${SO_S3_URL}"/`python -c "from uuid import uuid4;print(uuid4())"`;
-    COMMIT_HASH=`git rev-parse HEAD`;
-
-    pytest integration-tests/test_s3.py --benchmark-save="${COMMIT_HASH}";
-
-    aws s3 cp .benchmarks/*/*.json "${SO_S3_RESULT_URL}";
-    aws s3 rm --recursive "${SO_S3_URL}";
-  else
-    echo "[WARNING] Skip 'benchmark' testing"
+  if !is_travis_secure_vars_available; then
+    return 1
   fi
+
+  SO_S3_URL="${SO_S3_URL}"/`python -c "from uuid import uuid4;print(uuid4())"`;
+  COMMIT_HASH=`git rev-parse HEAD`;
+
+  pytest integration-tests/test_s3.py --benchmark-save="${COMMIT_HASH}";
+
+  aws s3 cp .benchmarks/*/*.json "${SO_S3_RESULT_URL}";
+  aws s3 rm --recursive "${SO_S3_URL}";
 }
 
 integration(){
   pytest integration-tests/test_http.py integration-tests/test_207.py
 
-  if is_travis_secure_vars_available; then
-    pytest integration-tests/test_s3_ported.py;
-  else
-    echo "[WARNING] Skip 'integration' testing"
+  if !is_travis_secure_vars_available; then
+    return 1
   fi
+
+  pytest integration-tests/test_s3_ported.py;
 }
 
 doctest(){
-  if is_travis_secure_vars_available; then
-    python -m doctest README.rst -v;
-  else
-    echo "[WARNING] Skip 'doctest' testing"
+  if !is_travis_secure_vars_available; then
+    return 1
   fi
+
+  python -m doctest README.rst -v;
 }
 
 
@@ -50,7 +51,7 @@ enable_moto_server(){
 }
 
 disable_moto_server(){
-  lsof -i tcp:5000 | tail -n1 | cut -f2 -d" " | xargs kill -9 || true
+  lsof -i tcp:5000 | tail -n1 | cut -f2 -d" " | xargs kill -9
 }
 
 "$@"
