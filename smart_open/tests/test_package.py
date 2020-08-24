3cdc9d800ace11b95a77b81617425e5462be03fe
justindujardin <justin@dujardinconsulting.com>
2020-08-24 09:52:17 -0700

    add tests for missing package import errors

---
+++ smart_open/tests/test_package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import unittest
+import pytest
+
+skip_tests: bool = "SMART_OPEN_TEST_MISSING_DEPS" not in os.environ
+
+
+class PackageTests(unittest.TestCase):
+
+    @pytest.mark.skipif(skip_tests, reason="requires missing dependencies")
+    def test_azure_raises_helpful_error_with_missing_deps(self):
+        with pytest.raises(ImportError, match=r"pip install smart_open\[azure\]"):
+            from smart_open import azure # noqa
+
+    @pytest.mark.skipif(skip_tests, reason="requires missing dependencies")
+    def test_aws_raises_helpful_error_with_missing_deps(self):
+        with pytest.raises(ImportError, match=r"pip install smart_open\[aws\]"):
+            from smart_open import s3 # noqa
+
+    @pytest.mark.skipif(skip_tests, reason="requires missing dependencies")
+    def test_gcs_raises_helpful_error_with_missing_deps(self):
+        with pytest.raises(ImportError, match=r"pip install smart_open\[gcs\]"):
+            from smart_open import gcs # noqa
--- travis_ci_helpers.sh
+++ travis_ci_helpers.sh
@@ -3,9 +3,6 @@
 set -e
 set -x
 
-export PYTEST_ADDOPTS="--reruns 3 --reruns-delay 1"
-
-
 is_travis_secure_vars_available(){
   if [[ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]]; then
     return 0
@@ -20,6 +17,7 @@ benchmark(){
     return 0
   fi
 
+  export PYTEST_ADDOPTS="--reruns 3 --reruns-delay 1"
   SO_S3_URL="${SO_S3_URL}"/`python -c "from uuid import uuid4;print(uuid4())"`;
   COMMIT_HASH=`git rev-parse HEAD`;
 
@@ -30,12 +28,17 @@ benchmark(){
 }
 
 integration(){
+  export PYTEST_ADDOPTS="--reruns 3 --reruns-delay 1"
   pytest integration-tests/test_http.py integration-tests/test_207.py
   if ! is_travis_secure_vars_available; then
     return 0
   fi
 
   pytest integration-tests/test_s3_ported.py;
+}
+
+dependencies(){
+  SMART_OPEN_TEST_MISSING_DEPS=1 pytest smart_open/tests/test_package.py -v --cov smart_open --cov-report term-missing;
 }
 
 doctest(){
