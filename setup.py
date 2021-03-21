f3923149ae5a279208484bf1da83b8ac938bb13b
Paul O'Leary McCann <polm@dampfkraft.com>
2021-03-21 21:20:46 +0900

    Remove remaining GCP references

--- setup.py
+++ setup.py
@@ -36,11 +36,11 @@ def read(fname):
     return io.open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 aws_deps = ['boto3']
-gcp_deps = ['google-cloud-storage']
+gcs_deps = ['google-cloud-storage']
 azure_deps = ['azure-storage-blob', 'azure-common', 'azure-core']
 http_deps = ['requests']
 
-all_deps = aws_deps + gcp_deps + azure_deps + http_deps
+all_deps = aws_deps + gcs_deps + azure_deps + http_deps
 tests_require = all_deps + [
     'mock',
     'moto[server]==1.3.14',  # Older versions of moto appear broken
