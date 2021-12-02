c3edcb2d8aec8e8784f8204af5710f3c25549781
Ron Korving <ron@ronkorving.nl>
2021-12-02 11:12:29 +0900

    Fix a changed boto3 documentation URL
    The URL about multiprocessing is now at https://boto3.amazonaws.com/v1/documentation/api/latest/guide/resources.html#multithreading-or-multiprocessing-with-resources

--- smart_open/s3.py
+++ smart_open/s3.py
@@ -1240,7 +1240,7 @@ def _download_key(key_name, bucket_name=None, retries=3, **session_kwargs):
         raise ValueError('bucket_name may not be None')
 
     #
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/resources.html#multithreading-and-multiprocessing
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/resources.html#multithreading-or-multiprocessing-with-resources
     #
     session = boto3.session.Session(**session_kwargs)
     s3 = session.resource('s3')
