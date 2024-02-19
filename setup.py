58f31a169e19f18f1298c9e9e370f2eb4f05731a
John Vandenberg <jayvdb@gmail.com>
2024-02-20 07:47:22 +0800

    Support moto 4 & 5 (#802)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ ssh_deps = ['paramiko']
 
 all_deps = aws_deps + gcs_deps + azure_deps + http_deps + ssh_deps
 tests_require = all_deps + [
-    'moto[server]<5.0',
+    'moto[server]',
     'responses',
     'boto3',
     'pytest',
