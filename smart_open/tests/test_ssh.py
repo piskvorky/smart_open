269c3a2ae6efe5176470a2e0810f107a4f1130d5
wbeardall <37509266+wbeardall@users.noreply.github.com>
2024-02-20 21:46:41 +0000

    Add support for SSH connection via aliases from `~/.ssh/config` (#790)
    * Added support for SSH/SCP/SFTP configuration from the default OpenSSH config file.
    
    * Added support for SSH/SCP/SFTP configuration from the default OpenSSH config file.
    
    * Added tests for override of user and port from config
    
    * Linting fixes
    
    * simplify and clean up tests
    
    * clean up ssh.py submodule
    
    * fix linter issues
    
    ---------
    
    Co-authored-by: Michael Penkov <m@penkov.dev>

--- smart_open/tests/test_ssh.py
+++ smart_open/tests/test_ssh.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
 import unittest
 from unittest import mock
 
 from paramiko import SSHException
 
 import smart_open.ssh
+
+_TEST_DATA_PATH = os.path.join(os.path.dirname(__file__), "test_data")
+_CONFIG_PATH = os.path.join(_TEST_DATA_PATH, "ssh.cfg")
 
 
 def mock_ssh(func):
@@ -20,6 +24,13 @@ def mock_ssh(func):
 
 
 class SSHOpen(unittest.TestCase):
+    def setUp(self):
+        self._cfg_files = smart_open.ssh._SSH_CONFIG_FILES
+        smart_open.ssh._SSH_CONFIG_FILES = [_CONFIG_PATH]
+
+    def tearDown(self):
+        smart_open.ssh._SSH_CONFIG_FILES = self._cfg_files
+
     @mock_ssh
     def test_open(self, mock_connect, get_transp_mock):
         smart_open.open("ssh://user:pass@some-host/")
@@ -67,6 +78,54 @@ class SSHOpen(unittest.TestCase):
         smart_open.open("ssh://user:pass@some-host/")
         mock_connect.assert_called_with("some-host", 22, username="user", password="pass")
         mock_sftp.open.assert_called_once()
+
+    @mock_ssh
+    def test_open_with_openssh_config(self, mock_connect, get_transp_mock):
+        smart_open.open("ssh://another-host/")
+        mock_connect.assert_called_with(
+            "another-host-domain.com",
+            2345,
+            username="another-user",
+            key_filename=["/path/to/key/file"],
+            timeout=20.,
+            compress=True,
+            gss_auth=False,
+            gss_kex=False,
+            gss_deleg_creds=False,
+            gss_trust_dns=False,
+        )
+
+    @mock_ssh
+    def test_open_with_openssh_config_override_port(self, mock_connect, get_transp_mock):
+        smart_open.open("ssh://another-host:22/")
+        mock_connect.assert_called_with(
+            "another-host-domain.com",
+            22,
+            username="another-user",
+            key_filename=["/path/to/key/file"],
+            timeout=20.,
+            compress=True,
+            gss_auth=False,
+            gss_kex=False,
+            gss_deleg_creds=False,
+            gss_trust_dns=False,
+        )
+
+    @mock_ssh
+    def test_open_with_openssh_config_override_user(self, mock_connect, get_transp_mock):
+        smart_open.open("ssh://new-user@another-host/")
+        mock_connect.assert_called_with(
+            "another-host-domain.com",
+            2345,
+            username="new-user",
+            key_filename=["/path/to/key/file"],
+            timeout=20.,
+            compress=True,
+            gss_auth=False,
+            gss_kex=False,
+            gss_deleg_creds=False,
+            gss_trust_dns=False,
+        )
 
 
 if __name__ == "__main__":
