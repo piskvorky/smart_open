4e338a22d292242c616c93b3ffbaa85b114a650e
ddelange <14880945+ddelange@users.noreply.github.com>
2025-10-20 14:17:46 +0200

    Prefill target branch and new tag on the new GitHub release page (#897)

--- release/release.sh
+++ release/release.sh
@@ -5,6 +5,10 @@ set -euxo pipefail
 
 read -p "Enter new version for the CHANGELOG.md header: " new_version
 
+# Ensure version has 'v' prefix
+if [[ ! "${new_version}" == v* ]]; then
+    new_version="v${new_version}"
+fi
 # Make sure you're on `develop` and you're up to date locally
 git checkout develop
 git pull
@@ -27,7 +31,7 @@ git pull
 git merge master --no-ff -m "Merge branch 'master' into develop"
 git push
 # Open the new GitHub Release page for convenience
-new_release_url="https://github.com/piskvorky/smart_open/releases/new"
+new_release_url="https://github.com/piskvorky/smart_open/releases/new?target=master&tag=${new_version}"
 echo "Follow release/README.md instructions: opening ${new_release_url}..."
 sleep 1
 xdg-open "${new_release_url}" || open "${new_release_url}"  # linux || macos
